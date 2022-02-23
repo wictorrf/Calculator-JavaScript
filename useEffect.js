d602020a0a0e5c36e77369281f14cfe0e6674fa2
wictorrf <wictor.santos20@gmail.com>
2022-02-23 20:35:10 -0300

    firt commit

---
+++ useEffect.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+
+function App() {
+
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar react hooks'
+  ]);
+
+  const [input, setInput] = useState('');
+
+  useEffect(()=>{
+    const tarefasStorage = localStorage.getItem('tarefas');
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+
+    return () => {};
+
+  }, []);
+
+  useEffect(()=> {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+  }, [tarefas])
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
+
+  
+
+  return (
+   <div>
+     <ul>
+        {tarefas.map(tarefa =>(
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+     </ul>
+     <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+     <button type="button" onClick={handleAdd} >Adicionar</button>
+   </div>
+  );
+}
+
+export default App;
---
+++ useMemo.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect, useMemo} from 'react';
+
+function App() {
+
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar react hooks'
+  ]);
+
+  const [input, setInput] = useState('');
+
+  useEffect(()=>{
+    const tarefasStorage = localStorage.getItem('tarefas');
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+
+    return () => {};
+
+  }, []);
+
+  useEffect(()=> {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+  }, [tarefas])
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
+
+  const totalTarefas = useMemo(()=> tarefas.length, [tarefas]);
+
+  
+
+  return (
+   <div>
+     <ul>
+        {tarefas.map(tarefa =>(
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+     </ul>
+          <br/>
+        <strong>voce tem {tarefas.length} tarefas!</strong><br/>
+     <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+     <button type="button" onClick={handleAdd} >Adicionar</button>
+   </div>
+  );
+}
+
+export default App;
